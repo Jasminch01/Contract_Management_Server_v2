9cb0096591472f437c22de98a194a65045d6155c
jasmin <chjasmin27@gmail.com>
2025-12-02 11:01:44 +0600

    create duplicate bug resolved

--- models/Buyer.js
+++ models/Buyer.js
@@ -1,23 +1,4 @@
 const mongoose = require('mongoose');
-
-
-// const buyerSchema = new mongoose.Schema({
-//     name:          { type: String, required: true },           // Buyer name
-//     abn:           { type: String },
-//     email:         { type: String },
-//     accountNumber: { type: String },
-//     officeAddress: { type: String },
-//     contactName:   {type : [String]},
-//     phoneNumber:   { type: String },
-//     isDeleted: { type: Boolean, default: false },
-//     deletedAt: { type: Date }
-
-//   },{ timestamps: true }
-// );
-
-// module.exports = mongoose.model('Buyer', buyerSchema);
-
-
 // Contact sub-schema for structured contact information
 const contactSchema = new mongoose.Schema({
     name: { type: String, required: true },
