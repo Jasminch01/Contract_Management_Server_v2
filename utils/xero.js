255dff9f506e448922d58b8b62529d49c224084b
jasmin <chjasmin27@gmail.com>
2025-12-04 11:12:20 +0600

    v2 check

--- utils/xero.js
+++ utils/xero.js
@@ -9,8 +9,8 @@ const xero = new XeroClient({
   // clientSecret: `3dqLHdc7NTar_wUlljHVPPPFXWhZFZdmBkjI3Ai_2e005TVC`,
   redirectUris: [
     // `http://localhost:8000/api/auth/xero/callback`,
-    // `https://contract-management-server-v2-1kq1.vercel.app/api/auth/xero/callback`,
-    `https://contract-management-server-tawny.vercel.app/api/auth/xero/callback`,
+    `https://contract-management-server-v2-1kq1.vercel.app/api/auth/xero/callback`,
+    // `https://contract-management-server-tawny.vercel.app/api/auth/xero/callback`,
   ],
   scopes: [
     "openid",
